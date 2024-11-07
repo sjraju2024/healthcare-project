3d08cba558b3362f26425df778e4b3c6ad7ba75c
sjraju2024 <sjraju2024@gmail.com>
2024-11-07 22:19:11 +0530

    Create main.tf

---
+++ Terraform/env/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source       = "./modules/vpc"
+  public_az   = var.public_az
+  private_az  = var.private_az
+}
+
+module "eks" {
+  source       = "./modules/eks"
+  cluster_name = var.cluster_name
+  subnet_ids   = [module.vpc.public_subnet_id, module.vpc.private_subnet_id]
+  desired_size = var.desired_size
+  max_size     = var.max_size
+  min_size     = var.min_size
+}
+
+module "ecr" {
+  source   = "./modules/ecr"
+  repo_name = var.repo_name
+}
